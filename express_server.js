b2f0ede06a4d769c3d6a230ee40d438cf3b8585c
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-16 23:06:48 +0000

    Added a random alphanumeric string generator and added new request

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; 
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+   return Math.random().toString(36).substr(4,6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,21 +17,32 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
    res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls", (req,res) => {
    let templateVars = { urls: urlDatabase}
    res.render("urls_index", templateVars)
 });
+
+app.get("/urls/new", (req, res) => {
+   res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
    res.render("urls_show", templateVars)
 })
-
+app.post("/urls", (req, res) => {
+   console.log(req.body);  // Log the POST request body to the console
+   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
