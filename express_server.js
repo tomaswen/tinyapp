75d56fa9db1b0aec03e1f7a28307ea4db7d985e6
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-16 21:59:14 +0000

    Added ejs response to code

--- express_server.js
+++ express_server.js
@@ -18,6 +18,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req,res) => {
+   let templateVars = { urls: urlDatabase}
+   res.render("urls_index", templateVars)
+});
+app.get("/urls/:shortURL", (req, res) => {
+   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+   res.render("urls_show", templateVars)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
