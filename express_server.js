677c60b2ce8e222719b014c03b038a35a4c773a1
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-17 18:07:41 +0000

    Added post/delete route to the server

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// GENERATES A RANDOM 6DIGIT STRING
 function generateRandomString() {
    let randomStr = '';
    let charString = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
@@ -14,52 +15,59 @@ function generateRandomString() {
    }
    return randomStr;
 };
+//----------------------URL-DATABASE---------------------
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//----------------------REQUESTS-------------------------
+
+//HOMEPAGE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+//SEND A JSON OF URLS IN THE HARDCODED DATABASE
 app.get("/urls.json", (req, res) => {
    res.json(urlDatabase);
 });
-
+//PRINTS HELLO WORLD
 app.get("/hello", (req, res) => {
    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+//SENDS A HTML RESPONSE OF THE URLS IN THE DATABASE
 app.get("/urls", (req,res) => {
    let templateVars = { urls: urlDatabase}
    res.render("urls_index", templateVars)
 });
-
+//SENDS HTML RESPONSE TO ADD NEW URL TO THE DATABASE
 app.get("/urls/new", (req, res) => {
    res.render("urls_new");
 });
-
+//SENDS HTML RESPONSE TO SHOW THE SPECIFIC SITE AND ITS SHORTENED URL
 app.get("/urls/:shortURL", (req, res) => {
    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
    res.render("urls_show", templateVars)
 });
-
+// AFTER POSTING/ADDING URL IT WILL REDIRECT BACK TO THE URL/:ID 
 app.post("/urls", (req, res) => {
    let newLongURL = req.body.longURL;
    let newShortURL = generateRandomString();
    urlDatabase[newShortURL] = newLongURL
-   console.log(urlDatabase)
    res.redirect(`/urls/${newShortURL}`);        
 });
-
+// WILL REDIRECT TO THE ACTUAL WEBSITE BY USING GET REQUEST /U/:SHORTURL
 app.get("/u/:shortURL", (req, res) => {
    const longURL = urlDatabase[req.params.shortURL]
-   console.log(longURL)
    res.redirect(longURL);
 });
-
+//AFTER POSTING/DELETE WILL DELETE KEY FROM THE DATABASE
+app.post("/urls/:shortURL/delete", (req, res) =>{
+   let shortURL = req.params.shortURL;
+   delete urlDatabase[shortURL];
+   res.redirect("/urls")
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
