5d74ccc4cc4bba31615101fc5172d41b84304d10
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-18 03:36:37 +0000

    Added login and register functions to tinyApp

--- express_server.js
+++ express_server.js
@@ -7,6 +7,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
+//----------------------URL-DATABASE---------------------
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//----------------------USER-DATABASE---------------------
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+//-----------------------FUNCTIONS--------------------------
 // GENERATES A RANDOM 6DIGIT STRING
 const generateRandomString = () => {
   let randomStr = '';
@@ -17,40 +40,57 @@ const generateRandomString = () => {
   }
   return randomStr;
 };
-//----------------------URL-DATABASE---------------------
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+//CHECKS IN THE EMAIL IS IN THE USERS DATABASE
+const checkIfEmailIsInUserDatabase = (database, emailToCheck) => {
+  for (let user in database) {
+    if (database[user].email === emailToCheck) {
+      return true;
+    }
+  }
+  return false;
 };
-
+// FETCH USER_ID FROM DATABASE USING EMAIL
+const fetchUserID = (database, email) => {
+  for (let user in database) {
+    if (database[user].email === email) {
+      return database[user].id;
+    }
+  }
+};
+//CHECKS IF PASSWORD MATCHES TO THE ONE IN DATABASE GIVEN AN ID
+const isPasswordCorrect = (database, id, password) => {
+  if (database[id].password === password) {
+    return true;
+  }
+  return false;
+};
 //----------------------REQUESTS-------------------------
 
 //HOMEPAGE
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
-//SEND A JSON OF URLS IN THE HARDCODED DATABASE
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-//PRINTS HELLO WORLD
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// //SEND A JSON OF URLS IN THE HARDCODED DATABASE
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// //PRINTS HELLO WORLD
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 //SENDS A HTML RESPONSE OF THE URLS IN THE DATABASE
 app.get("/urls", (req,res) => {
-  let templateVars = { urls: urlDatabase , username: req.cookies["username"]};
+  let templateVars = { urls: urlDatabase , users: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
 });
 //SENDS HTML RESPONSE TO ADD NEW URL TO THE DATABASE
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"]};
+  let templateVars = { users: users[req.cookies["user_id"]]};
   res.render("urls_new",templateVars);
 });
 //SENDS HTML RESPONSE TO SHOW THE SPECIFIC SITE AND ITS SHORTENED URL
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], users: users[req.cookies["user_id"]]};
   res.render("urls_show", templateVars);
 });
 // AFTER POSTING/ADDING URL IT WILL REDIRECT BACK TO THE URL/:ID
@@ -79,14 +119,52 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 //AFTER POST LOGIN
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  const userID = fetchUserID(users, userEmail);
+  if (!userID) {
+    res.send("not found");
+  } else if (!isPasswordCorrect(users,userID,userPassword)) {
+    res.send("Password incorrect");
+  } else {
+    res.cookie("user_id", userID);
+    res.redirect("/urls");
+  }
 });
 //AFTER LOGOUT
-app.post("/logout", (req, res) =>{
-  res.clearCookie("username");
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
+//REGISTER PAGE
+app.get("/register", (req, res) => {
+  let templateVars = { users: users[req.cookies["user_id"]]};
+  res.render("registration_form", templateVars);
+});
+//POST /REGISTER USER INTO THE DATABASE
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    res.statusCode = 400;
+    res.redirect("/urls");
+  }
+  if (checkIfEmailIsInUserDatabase(users,req.body.email)) {
+    res.statusCode = 400;
+    res.redirect("/urls");
+  } else {
+    let userID = generateRandomString();
+    users[userID] = req.body;
+    users[userID].id = userID;
+    res.cookie("user_id", userID);
+    res.redirect("/urls");
+  }
+});
+//LOGIN PAGE
+app.get("/login", (req, res) => {
+  let templateVars = {users: users[req.cookies["user_id"]]};
+  res.render("login_form", templateVars);
+});
+
+//----------------------LISTEN------------------------------
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
